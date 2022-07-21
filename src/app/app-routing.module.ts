55e92f9bd9465b87b6937cc18ca32b0fa2f20725
waseem.isaac@afaqy.com <waseem.isaac@afaqy.com>
2022-07-21 12:19:16 +0200

    Init Team module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
-  Shell.childRoutes([{ path: 'reports', loadChildren: () => import('./reports/reports.module').then((m) => m.ReportsModule) }]),
+  Shell.childRoutes([
+    { path: 'reports', loadChildren: () => import('./reports/reports.module').then((m) => m.ReportsModule) },
+    { path: 'team', loadChildren: () => import('./team/team.module').then((m) => m.TeamModule) }
+
+  ]),
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/team/team.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Task , Project, RegisterContext} from '@app/@shared/interfaces';
+import { CredentialsService } from '@app/auth';
+import * as moment from 'moment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+  constructor(private _http: HttpClient, private credentialsService: CredentialsService) { }
+
+  getTeam(): Observable<RegisterContext[]>{
+    return this._http.get<RegisterContext[]>('users')
+  }
+
+  getMemberTasksNumber(userId: string){
+    return this._http.get<RegisterContext[]>('tasks/user/'+userId)
+  }
+}
