49c5978d2bd898cd9862392d124bca836830d820
waseem.isaac@afaqy.com <waseem.isaac@afaqy.com>
2022-07-26 16:25:50 +0200

    Handle invalid auth token

--- src/app/@shared/http/error-handler.interceptor.ts
+++ src/app/@shared/http/error-handler.interceptor.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
+import { AuthenticationService } from '@app/auth';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 /**
@@ -13,6 +16,7 @@ import { environment } from '@env/environment';
   providedIn: 'root',
 })
 export class ErrorHandlerInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthenticationService, private router: Router, private _snackBar: MatSnackBar) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError((error) => this.errorHandler(error)));
   }
@@ -23,6 +27,20 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
       // Do something with the error
       console.log('Request error', response);
     }
+
+    if ((response instanceof HttpErrorResponse) && response['status'] === 401) {
+      this.authService.logout().subscribe(() => {
+
+        this._snackBar.open(response?.error?.message || 'Email or password are incorrect', 'Ok', {
+          panelClass: ['custom-snackbar'],
+          horizontalPosition: 'left',
+          verticalPosition: 'bottom'
+        });
+
+        this.router.navigate(['/login'], { replaceUrl: true })
+      });
+    }
+
     throw response;
   }
 }
